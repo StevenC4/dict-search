4d6b2a841ce4e264b88b4a48634ed391bc094567
Steve Carroll <stevencarroll90@gmail.com>
2023-01-29 23:11:47 -0700

    Adding Trie with add word functionality

---
+++ src/trie.ts
@@ -0,0 +1,54 @@
+export class Trie {
+	head: TrieNode;
+
+	constructor(contents: Array<string>) {
+		this.head = new TrieNode();
+		if (!!contents) {
+			this.load(contents);
+		}
+	}
+
+	load(contents: Array<string>): void {
+
+	}
+
+	add(word: string): boolean {
+		const lcWord = word.toLowerCase().trim();
+		if (!this.has(word)) {
+			this.head.add(lcWord);
+			return true;
+		}
+		return false;
+	}
+
+	has(word: string): boolean {
+		const lcWord = word.toLowerCase().trim();
+		if (lcWord === '') {
+			return false;
+		}
+		return false;
+	}
+}
+
+export class TrieNode {
+	isCompleteWord: boolean;
+	children: Array<string>;
+
+	constructor() {
+		this.isCompleteWord = false;
+		this.children = [];
+	}
+
+	add(word: string): void {
+		if (!!word.length) {
+			this.isCompleteWord = true;
+		} else {
+			const firstLetter = word[0];
+			const restOfWord = word.substring(1);
+			if (!(firstLetter in this.children)) {
+				this.children[firstLetter] = new TrieNode();
+			}
+			this.children[firstLetter].add(this.children);
+		}
+	}
+}
